12ec6c73263a7397abdd201ab00917b6884531f4
samira-2002-code <samiraassemlali2@gmail.com>
2026-04-16 12:08:08 +0100

    creation of table students

--- script.sql
+++ script.sql
@@ -14,3 +14,29 @@ CREATE TABLE users(
     role_id INT NOT NULL,
     FOREIGN KEY (role_id) REFERENCES roles(id)
 );
+CREATE Table classes(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    class_number VARCHAR(20) 
+);
+
+CREATE Table coursses(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    tittle VARCHAR(255) NOT NULL,
+    description TEXT ,
+    total_hours INT,
+    user_id INT,
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+CREATE Table students(
+id INT PRIMARY KEY AUTO_INCREMENT,
+dateofbirth date NOT NULL,
+student_number VARCHAR(255),
+user_id INT NOT NULL,
+Foreign Key (user_id) REFERENCES users(id),
+class_id INT NOT NULL,
+Foreign Key (class_id) REFERENCES classes(id)
+);
+
+ 
